472a14c11799ae2dc1aeb28c6b7e8550ce6d5a67
Andy Truong <thehongtt@gmail.com>
2016-07-19 11:26:48 +0700

    A bit better regex.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ vars=$(echo $vars | tr "\n")
 
 for var in $vars
 do
-    key=$(echo "$var" | sed -E 's/_DOCKER_([A-Z]+)=.+/\1/g')
-    var=$(echo "$var" | sed -E 's/_DOCKER_([A-Z]+)=.+/_DOCKER_\1/g')
+    key=$(echo "$var" | sed -E 's/_DOCKER_([^=]+).+/\1/g')
+    var=$(echo "$var" | sed -E 's/_DOCKER_([^=]+).+/_DOCKER_\1/g')
     eval val=\$$var
     sed -i "/fastcgi_param SERVER_NAME \$host;/a fastcgi_param $key \"$val\";" /etc/nginx/conf.d/app.conf
 done
